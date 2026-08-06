097814756e16e0a602183abd152045cef7a2d9f8
Aryan Yadav <22garyan@gmail.com>
2026-08-06 10:50:33 +0530

    role fixes

--- src/data/team.ts
+++ src/data/team.ts
@@ -4,8 +4,8 @@ export interface TeamMember {
 }
 
 export const team: TeamMember[] = [
-  { name: 'Somnath Halder', role: 'CEO' },
-  { name: 'Manish Kumar', role: 'COO' },
-  { name: 'Aryan', role: 'CTO' },
-  { name: 'Sancheeta', role: 'Software Engineer' },
+  { name: 'Somnath Halder', role: 'Founder & CEO' },
+  { name: 'Manish Kumar', role: 'Co-Founder & COO' },
+  { name: 'Aryan', role: 'Co-Founder & CTO' },
+  { name: 'Sancheeta', role: 'Intern' },
 ]
